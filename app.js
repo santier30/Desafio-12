432c1460ed0eae4ef4855bd79dfa3cd666107c91
Santiago Hryb <santihryyb@gmail.com>
2023-09-17 17:40:10 -0300

    Add email and age-Add modal for edits

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const section = document.getElementById('section');
 const name = document.getElementById('name');
 const number = document.getElementById('number');
+const email = document.getElementById('Email');
+const age = document.getElementById('Age');
 const contact = document.getElementById('Add');
+
+const nameEdit = document.getElementById('nameEdit');
+const numberEdit = document.getElementById('numberEdit');
+const emailEdit = document.getElementById('EmailEdit');
+const ageEdit = document.getElementById('AgeEdit');
+const contactEdit = document.getElementById('Edit');
+
 const newButton = document.getElementById('New');
 const cancel = document.getElementById('cancel');
 const searchInput = document.getElementById('form1');
 const searchButton = document.getElementById('searchButton');
 let display = false;
 let data="";
+let editId="";
 let last="";
+
+function editModalHandler(mark){
+    nameEdit.value=data[mark].name;
+    numberEdit.value=data[mark].number;
+    ageEdit.value=data[mark].age;
+    emailEdit.value=data[mark].email;
+    editId=data[mark].id;
+}
+
+
 function displayHandler(dataF){
     section.innerHTML=dataF!=""?"":`<h1 class="text-white text-center">Agrege algun contacto</h1`
       for(element in dataF){
-          section.innerHTML +=createArticleHandler(dataF[element])
+          section.innerHTML +=createArticleHandler(dataF[element],element)
       }
 last="";
 }
@@ -21,7 +41,7 @@ function searchHandler(){
     let s =[...data].filter((element)=>regex.test(element.name))
     displayHandler(s)
 }
-function createArticleHandler(data){
+function createArticleHandler(data,mark){
     if(last!=data.name[0].toUpperCase()) {
         last = data.name[0].toUpperCase();
         return`
@@ -29,6 +49,9 @@ function createArticleHandler(data){
         <article class="bg-light p-2 d-flex justify-content-between text-white rounded">
         <h2>${data.name}</h2>
         <span class="p-2">${data.number}</span>
+        <span class="p-2">${data.age?data.age:''}</span>
+        <span class="p-2">${data.email?data.email:''}</span>
+        <button type="button" class="btn btn-warning text-white me-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onClick="editModalHandler('${mark}')">Editar</button>
         <button class="btn btn-warning text-white" onClick="deleteHandler('${data.id}')">Borrar</button>
         </article>
         `
@@ -37,6 +60,9 @@ function createArticleHandler(data){
         <article class="bg-light p-2 d-flex justify-content-between text-white rounded">
         <h2>${data.name}</h2>
         <span class="p-2">${data.number}</span>
+        <span class="p-2">${data.age?data.age:''}</span>
+        <span class="p-2">${data.email?data.email:''}</span>
+        <button type="button" class="btn btn-warning text-white me-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onClick="editModalHandler('${mark}')">Editar</button>
         <button class="btn btn-warning text-white" onClick="deleteHandler('${data.id}')">Borrar</button>
         </article>
         `
@@ -46,6 +72,9 @@ function createArticleHandler(data){
 function clear(){
     name.value = '';
     number.value = '';
+    age.value = '';
+    email.value = '';
+    
 }
 function displayAddHandler() {
 if (!display){
@@ -63,7 +92,12 @@ async function postHandler(event){
     event.preventDefault();
     if(name.value && number.value){
         try{
-       const response = await fetch("https://contactos-cfb7b-default-rtdb.firebaseio.com/Contactos.json", {method: 'POST', body:JSON.stringify({name: name.value, number: number.value})})
+       const response = await fetch("https://contactos-cfb7b-default-rtdb.firebaseio.com/Contactos.json", {method: 'POST', body:JSON.stringify({
+        name: name.value,
+         number: number.value,
+         email:email.value,
+         age: age.value
+    })})
        if(!response.ok){throw new Error("Posting Error")}
        await fetchHandler()
        displayAddHandler()
@@ -74,6 +108,20 @@ async function postHandler(event){
 function deleteHandler(id){
     data=""
     fetch(`https://contactos-cfb7b-default-rtdb.firebaseio.com/Contactos/${id}.json`,{method: 'DELETE'}).then(()=>fetchHandler())
+}
+function editHandler(event){
+    event.preventDefault()
+    if(nameEdit.value && numberEdit.value){
+        data=""
+        fetch(`https://contactos-cfb7b-default-rtdb.firebaseio.com/Contactos/${editId}.json`,{method: 'put',body:JSON.stringify({
+            name: nameEdit.value,
+            number: numberEdit.value,
+            email:emailEdit.value,
+            age: ageEdit.value
+    
+        })}).then(()=>fetchHandler())
+    }
+
 }
 
 async function fetchHandler(){
@@ -98,3 +146,4 @@ contact.addEventListener("submit",postHandler)
 newButton.addEventListener("click",displayAddHandler)
 cancel.addEventListener("click",displayAddHandler)
 searchButton.addEventListener("click",searchHandler)
+contactEdit.addEventListener("submit",editHandler)
